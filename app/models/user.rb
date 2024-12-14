7a206a693c0fe49a3c585f268bd899625b5aca9c
AndrewRiabets <Inferion5@gmail.com>
2024-12-14 18:52:07 +0200

    example custom validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,3 +14,13 @@ class User < ApplicationRecord
       length: { minimum: 8, message: "must be at least 8 characters long" },
       format: { with: /\A(?=.*[!@#$%^&*])(?!.*(.)\1{2,}).*\z/, message: "must contain at least one special character and must not have more than two consecutive identical characters" }
 end
+
+
+#   validate :validate_no_consecutive_characters
+
+#     # Password must not have more than two consecutive identical characters
+#     def validate_no_consecutive_characters
+#       if password.present? && password.match(/(.)\1{2,}/)
+#         errors.add(:password, "must not have more than two consecutive identical characters")
+#       end
+#      end
