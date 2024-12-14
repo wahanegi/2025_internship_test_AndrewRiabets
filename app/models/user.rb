0fed1da4ab6d25ddb8a7586656f74896d292fd3e
AndrewRiabets <Inferion5@gmail.com>
2024-12-14 18:08:36 +0200

    added password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,9 @@ class User < ApplicationRecord
       presence: true,
       uniqueness: { case_sensitive: true },
       format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email format" }
+
+  validates :password,
+      presence: true,
+      length: { minimum: 8, message: "must be at least 8 characters long" },
+      format: { with: /\A(?=.*[!@#$%^&*])(?!.*(.)\1{2,}).*\z/, message: "must contain at least one special character and must not have more than two consecutive identical characters" }
 end
