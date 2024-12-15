9a30bf4d99c622e2d797e50dcc261fcada1ba65d
AndrewRiabets <Inferion5@gmail.com>
2024-12-15 14:53:50 +0200

    React-initialisation

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,8 @@
 class WelcomeController < ApplicationController
   def index
     if user_signed_in?
-      render "hello_user"
+      # render "hello_user"
+      redirect_to home_path
     else
       render "index"
     end
---
+++ app/javascript/components/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('react-root');
+  if (container) {
+    const root = createRoot(container);
+    root.render(<App />);
+  } else {
+    console.error('React root container not found!');
+  }
+});
